dbc0d4b39a19347742e296b7331498d81350ec0d
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-01-02 20:37:20 -0500

    updated after rewrite. Added add_empty_third_idx

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from pathlib import Path
 from argparse import ArgumentParser as argParser
 import os
-from tt_file_tools.file_tools import read_df, write_df, print_file_exists
 from functools import reduce
 
 import pandas as pd
+from tt_file_tools.file_tools import read_df, write_df, print_file_exists
+
+def add_empty_third_idx(frame: pd.DataFrame):
+    insert_row_after = [i for i in range(frame.index[-1]) if frame.iloc[i]['idx'] == 2 and frame.iloc[i + 1]['idx'] == 1]
+    if frame.iloc[-1]['idx'] == 2:
+        insert_row_after.append(len(frame) - 1)
+    for row_num in insert_row_after:
+        frame.loc[len(frame)] = {'idx': 3, 'date': frame.iloc[row_num]['date'], 'speed': frame.iloc[row_num]['speed']}
+    frame.sort_values(by=['date', 'speed', 'idx']).reset_index(inplace=True)
+    return frame
 
 if __name__ == '__main__':  #
 
     ap = argParser()
     ap.add_argument('filepath', type=str, help='path to arcs files')
-    ap.add_argument('year', type=str, help='year to aggregate')
     args = vars(ap.parse_args())
 
+    tt = 'transit times'
     path = Path(args['filepath'])
-    files = [file for file in os.listdir(Path(args['filepath'])) if args['year'] in file]
-    paths = [path.joinpath(file).joinpath('Transit Times').joinpath(file.split('_')[0] + '_transit_times.csv') for file in files]
-    frames = [read_df(path) for path in paths if print_file_exists(path)]
+    paths = [Path(os.path.join(p, f)) for p, _, fs in os.walk(Path(args['filepath'])) for f in fs if tt in f]
+    frames = [add_empty_third_idx(read_df(p)) for p in paths if print_file_exists(p)]
     aggregate_frame = reduce(lambda left, right: pd.merge(left, right, on=['idx', 'date', 'speed']), frames)
     write_df(aggregate_frame, path.joinpath('aggregate.csv'))
